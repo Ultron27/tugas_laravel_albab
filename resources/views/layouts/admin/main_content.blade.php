2327e5f133250548d3e8d7859450bf880cb8be54
Ultron27 <autonaja.14@gmail.com>
2019-04-10 08:39:08 +0700

    commit albab

---
+++ resources/views/layouts/admin/main_content.blade.php
@@ -0,0 +1,3 @@
+<section class="content">
+      @yield('isi')
+    </section>
---
+++ resources/views/layouts/admin/sidebar.blade.php
@@ -0,0 +1,49 @@
+<section class="sidebar">
+      <ul class="sidebar-menu" data-widget="tree">
+        <li class="active treeview">
+          <a href="#">
+            <i class="fa fa-dashboard"></i> <span>Dashboard</span>
+          </a>
+        </li>
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-pie-chart"></i>
+            <span>Pengguna</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="tambah_admin"><i class="fa fa-circle-o"></i> Admin</a></li>
+            <li><a href="tambah_distributor"><i class="fa fa-circle-o"></i> Distributor</a></li>
+            <li><a href="tambah_petugas"><i class="fa fa-circle-o"></i> Petugas</a></li>
+          </ul>
+        </li>
+        </li>
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-edit"></i> <span>Data Barang</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="tambah_barang"><i class="fa fa-circle-o"></i> Tambah Barang</a></li>
+            <li><a href="detail_barang"><i class="fa fa-circle-o"></i> Detail Barang</a></li>
+            <li><a href="tambah_jenis"><i class="fa fa-circle-o"></i> Jenis Barang</a></li>
+          </ul>
+        </li>
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-table"></i> <span>Data Penjualan</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="pages/tables/simple.html"><i class="fa fa-circle-o"></i> Detail Penjualan</a></li>
+            <li><a href="pages/tables/data.html"><i class="fa fa-circle-o"></i> Data tables</a></li>
+          </ul>
+        </li>
+      </ul>
+    </section>
